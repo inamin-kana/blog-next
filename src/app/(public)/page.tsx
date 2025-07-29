d612b69f3639cad87ae23966915567e943062deb
Kanako Inamine <inakana.oki08@gmail.com>
2025-07-29 19:20:50 +0200

    dev card section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,19 @@
+import { getPosts } from "@/lib/post"
+import PostCard from "@/components/ui/post/PostCard"
+import { Post } from "@/types/post"
 
-export default function postPage() {
+export default async function postPage() {
+  const posts = await getPosts() as Post[]
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols2 lg:grid-cols-3 gap-6">
+          {posts.map((post) =>(
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
